8f2ffcf15ddc78beed7d9c955f1e877b30457271
Özgür ÇOKÇEKEN <ozgur.cokceken@istinye.edu.tr>
2023-01-30 13:42:17 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [points, setPoints] = useState([])
+  const [history, setHistory] = useState([])
+
+  const mouseClickHandle = (e) => {
+    const { clientX, clientY } = e
+    setPoints([
+      ...points,
+      {
+        clientX,
+        clientY
+      }])
+  }
+
+  const handleUndo = () => {
+    const newPoints = [...points]
+    const deletedPoint = newPoints.pop()
+    setHistory([...history, deletedPoint])
+    setPoints([...newPoints])
+  }
+
+  const handleRedo = () => {
+    const newHistory = [...history]
+    const deletedPoint = newHistory.pop()
+    setHistory([...newHistory])
+    setPoints([...points, deletedPoint])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button
+        className='btn'
+        onClick={handleUndo}
+        disabled={points.length <= 0}>
+        Undo
+      </button>
+
+      <button
+        className='btn'
+        onClick={handleRedo}
+        disabled={history.length <= 0}>
+        Redo
+      </button>
+
+      <div className="App" onClick={mouseClickHandle}>
+        {
+          points.map((point, idx) => (
+            <div
+              key={idx}
+              className='point'
+              style={{
+                left: point.clientX + "px",
+                top: point.clientY + "px"
+              }}>
+            </div>
+          ))
+        }
+      </div>
+    </>
   );
 }
 
